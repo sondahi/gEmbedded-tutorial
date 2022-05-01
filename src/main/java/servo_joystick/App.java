24d8a2c320ce5df2ede4be14c01b64962d35f2d3
sondahi <gokay.comert@icloud.com>
2022-05-01 10:46:00 +0200

    servo-joystick

---
+++ src/main/java/servo_joystick/App.java
@@ -0,0 +1,67 @@
+package servo_joystick;
+
+import com.comert.gEmbedded.api.device.Device;
+import com.comert.gEmbedded.api.device.common.exception.IOException;
+import com.comert.gEmbedded.api.device.factory.DeviceFactory;
+import com.comert.gEmbedded.api.device.gpio.factory.GPIOFactory;
+import com.comert.gEmbedded.api.device.gpio.pin.PWMPin;
+import com.comert.gEmbedded.api.device.gpio.pin.Pin;
+import com.comert.gEmbedded.api.device.gpio.pin.PinFunction;
+import com.comert.gEmbedded.api.device.gpio.pin.configurator.PWMPinConfigurator;
+import com.comert.gEmbedded.api.device.i2c.factory.I2CMasterFactory;
+import com.comert.gEmbedded.api.device.i2c.master.I2CBus;
+import com.comert.gEmbedded.api.device.i2c.master.I2CMaster;
+import com.comert.gEmbedded.api.device.i2c.master.configurator.I2CMasterConfigurator;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        Device device = DeviceFactory.getDeviceInstance();
+        try {
+            device.setUpDevice();
+
+            final var sdaPin = Pin.PIN_2;
+            final var sclPin = Pin.PIN_3;
+            final byte slaveAddress = 0x4b;
+            final byte channel7 = (byte) 0b1_111_00_00;
+            I2CMasterFactory i2CMasterFactory = device.getI2CMasterFactoryInstance();
+            I2CMaster master = i2CMasterFactory.createI2CMaster(
+                    I2CMasterConfigurator
+                            .getBuilder()
+                            .bus(I2CBus.BUS_1)
+                            .sdaPin(sdaPin, PinFunction.ALT0)
+                            .sclPin(sclPin, PinFunction.ALT0)
+                            .busClockInHertz(3400000)
+                            .busClockStretchTimeout(0x0040)
+                            .build()
+            );
+
+            final var pwmPin = Pin.PIN_21;
+            GPIOFactory gpioFactory = device.getGPIOFactoryInstance();
+            PWMPin servo = gpioFactory.createPWMPin(
+                    PWMPinConfigurator
+                            .getBuilder()
+                            .softwarePWM()
+                            .pin(pwmPin)
+                            .build()
+            );
+
+            final var playTime = 1000;
+            for (int i = 0; i < playTime; i++) {
+                try {
+                    final int degree = master.readRegister(slaveAddress, channel7);
+                    servo.pulse(20000, (int) (500 + (degree * 7.84)));
+                } catch (IOException ioException) {
+                    System.out.println(ioException.getMessage());
+                }
+            }
+
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            throw throwable;
+        } finally {
+            device.shutDownDevice();
+        }
+    }
+
+}
